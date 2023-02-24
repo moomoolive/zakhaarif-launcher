0e514b2a383e194e464ec873ecfe8525e334765a
moomoo <mostafaelbannan1@gmail.com>
2023-02-24 07:39:28 -0800

    added cloudflare headers file to deployment

---
+++ src/build/headers.mjs
@@ -0,0 +1,18 @@
+import fs from "fs/promises"
+
+const HEADERS_FOR_ALL_URLS = "/*"
+
+const file = `
+${HEADERS_FOR_ALL_URLS}
+    Cross-Origin-Embedder-Policy: require-corp;
+    Cross-Origin-Opener-Policy: same-origin;
+    Cross-Origin-Resource-Policy: cross-origin;
+    X-Frame-Options: deny;
+`.trim()
+
+const CLOUDFLARE_HEADER_FILE = "_headers"
+console.log("creating deployment headers...")
+
+await fs.writeFile(`dist/${CLOUDFLARE_HEADER_FILE}`, file)
+
+console.info(`successfully created headers!`)
