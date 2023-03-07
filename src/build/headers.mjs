294d7882d77205cfb980fad6273a948cf1a8bbbd
moomoo <mostafaelbannan1@gmail.com>
2023-03-06 20:54:05 -0800

    removed restrictive cors

--- src/build/headers.mjs
+++ src/build/headers.mjs
@@ -2,14 +2,13 @@ import fs from "fs/promises"
 
 const DEV_DEPLOYMENT_ORIGIN = process.env.DEV_DEPLOYMENT_ORIGIN || "none"
 const MAIN_ORIGIN = process.env.MAIN_ORIGIN || "none"
-const SANDBOX_ORIGIN = process.env.SANDBOX_ORIGIN || "none"
 
 const HEADERS_FOR_ALL_URLS = "/*"
 const HEADERS_FOR_CLOUDFLARE_DEV_DEPLOYMENTS = "https://:project.pages.dev/*"
 
 const file = `
 ${HEADERS_FOR_ALL_URLS}
-    Vary: Origin
+    Access-Control-Allow-Origin: *
     ! X-Content-Type-Options
 
 ${HEADERS_FOR_CLOUDFLARE_DEV_DEPLOYMENTS}
@@ -22,15 +21,6 @@ ${MAIN_ORIGIN}/
     Cross-Origin-Embedder-Policy: require-corp
     Cross-Origin-Opener-Policy: same-origin
     X-Frame-Options: deny
-
-${MAIN_ORIGIN}/*
-    Access-Control-Allow-Origin: ${MAIN_ORIGIN}
-
-https://:deployment.${DEV_DEPLOYMENT_ORIGIN}/*
-    Access-Control-Allow-Origin: https://:deployment.${DEV_DEPLOYMENT_ORIGIN}
-
-${SANDBOX_ORIGIN}/*
-    Access-Control-Allow-Origin: ${SANDBOX_ORIGIN}
 `.trim()
 
 
